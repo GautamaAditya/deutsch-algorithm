232bb37ae6b744142de05c053cb04e30d58c9c9e
Gautama Aditya <adigautama1010@gmail.com>
2026-07-26 10:45:54 +0530

    corrected code output

--- deutsch_algorithm.py
+++ deutsch_algorithm.py
@@ -12,11 +12,11 @@ def create_oracle(function_choice):
     elif function_choice == 2:
         qc.cx(0,1)
         qc.x(1)
-    else:
+    elif function_choice == 3:
         qc.x(1)
 
 def print_function_type(circuit_output): #Argument of this function is known to the user but not to the algorithm
-    return "constant" if circuit_output == {'0': 1024} else "balanced"
+    return "constant" if circuit_output == {'0': 1} else "balanced"
 
 #Build quantum circuit
 qc = QuantumCircuit(2,1)
@@ -24,7 +24,7 @@ qc.h(0)
 qc.x(1)
 qc.h(1)
 
-create_oracle(2) 
+create_oracle(2) #change parameter to change function
 
 #Last stage of circuit
 qc.h(0)
@@ -34,7 +34,7 @@ qc.measure(0,0)
 #Deutsch's algorithm returns function type with probability 1. So one measurement is enough (one shot)
 result = AerSimulator().run(qc, shots = 1).result() 
 
-print(result.get_counts())
+# print(result.get_counts())
 print("The function is", print_function_type(result.get_counts()))
 qc.draw('mpl')
 plt.show()
